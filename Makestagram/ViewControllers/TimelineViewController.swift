22b3a6d38ffeb4fb014687af3cfbf3d6be89b91c
eurachoi <eurachoi@Mees-MacBook-Air.local>
2016-06-28 10:14:08 -0700

    photo

--- Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+var photoTakingHelper: PhotoTakingHelper?
 
 class TimelineViewController: UIViewController {
     
@@ -23,10 +25,18 @@ extension TimelineViewController: UITabBarControllerDelegate {
     
     func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
         if (viewController is PhotoViewController) {
-            print("Take Photo")
+            takePhoto();
             return false
         } else {
             return true
+        }
+    }
+    func takePhoto() {
+        // instantiate photo taking class, provide callback for when photo is selected
+        photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!) { (image: UIImage?) in
+            let post = Post()
+            post.image = image
+            post.uploadPost()
         }
     }
 }
